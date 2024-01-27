dc5693ca775a0e4de8ef3f4979eb50c1702948d5
Ethan <59590426+Blue-Berri@users.noreply.github.com>
2024-01-26 19:39:29 -0800

    everything

---
+++ backend/userData.js
@@ -0,0 +1,10 @@
+var users = [
+	{ // Object @ 0 index
+		username: "turtlecode",
+		password: "codechannel"
+	},
+	{ // Object @ 1 index
+		username: "monkeycode",
+		password: "codechannel"
+	}
+] 
