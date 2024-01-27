a83b69aa70537e3feb4703948709cbe0bb5bf0b3
Justin Ho <justin.ho05072005@gmail.com>
2024-01-26 23:19:36 -0800

    chatgpt matching almost done

--- backend/chatgpt modules.py
+++ backend/chatgpt modules.py
@@ -1,18 +1,24 @@
 from openai import OpenAI
-client = OpenAI(api_key="")
+import json 
+client = OpenAI(api_key="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")
+data = {}
+with open("backend/userData.json", "r") as file:
+    data = json.load(file)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {
       "role": "system", 
-      "content": "You are a matchmaker. I will you a set of traits about a person and give you a list of possible matches. your job is to find the best matches for the person. Refer to the use in the you form. Format entire response in the JSON format: { results: {name}, {age}, {gender}, {sexual orientation}, {major},  {bio} }" 
+      "content": f'You are a matchmaker. I will you a set of traits about a person and give you a list of possible matches in the json file {data}. your job is to find the best matches for the person. Refer to the use in the you form. Format entire response in the JSON format:  results: name, age, gender, sexual orientation, major,  bio ' 
      },
     {
       "role": "user", 
-      "content": "Me: 18, Male, Male, Computer Science,  I love to code! "
+      "content": "Me: 18, Male, Male, Computer Science,  I love to code!"
     }
   ]
 )
+
+results = completion.choices[0].message
 
 print(completion.choices[0].message)
