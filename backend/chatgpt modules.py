6aec4ed356335faa49da1ec692757cdf92b464ef
Justin Ho <justin.ho05072005@gmail.com>
2024-01-27 00:14:56 -0800

    changed chatgpt module and more data

--- backend/chatgpt modules.py
+++ backend/chatgpt modules.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 import json 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 data = {}
 with open("backend/userData.json", "r") as file:
     data = json.load(file)
@@ -10,11 +10,11 @@ completion = client.chat.completions.create(
   messages=[
     {
       "role": "system", 
-      "content": f'You are a matchmaker. I will you a set of traits about a person and give you a list of possible matches in the json file {data}. your job is to find the best matches for the person. Refer to the use in the you form. Format entire response in the JSON format:  results: name, age, gender, sexual orientation, major,  bio ' 
+      "content": f'You are a matchmaker. I will you a set of traits about me and give you a list of possible matches in the json file {data}. Match gender first. Then orientation. Then match around the same age. Then similar bio. Then major your job is to find the best matches for the person. Go through the whole list. Refer to the use in the you form. Format entire response in the JSON format:  results: name, age, gender, sexual orientation, major,  bio ' 
      },
     {
       "role": "user", 
-      "content": "Me: 18, Male, Male, Computer Science,  I love to code!"
+      "content": "Me: 18, Male, Straight, Computer Science,  I love to code!"
     }
   ]
 )
