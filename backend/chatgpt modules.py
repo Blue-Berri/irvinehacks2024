fc25fe7e3e274a5f2f7039b18f44c917a53af124
Justin Ho <justin.ho05072005@gmail.com>
2024-01-26 22:45:59 -0800

    changing chatgpt modules

--- backend/chatgpt modules.py
+++ backend/chatgpt modules.py
@@ -1,15 +1,18 @@
 from openai import OpenAI
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="")
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", 
-     "content": "You are a matchmaker. I will you a set of traits about a person and give you a list of possible mathces."
-     "your job is to find the best matches for the person. Refer to the use in the you form. Format entire response in the JSON format: { results: name, age, major, gender, sexual orientation, bio }" },
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {
+      "role": "system", 
+      "content": "You are a matchmaker. I will you a set of traits about a person and give you a list of possible matches. your job is to find the best matches for the person. Refer to the use in the you form. Format entire response in the JSON format: { results: {name}, {age}, {gender}, {sexual orientation}, {major},  {bio} }" 
+     },
+    {
+      "role": "user", 
+      "content": "Me: 18, Male, Male, Computer Science,  I love to code! "
+    }
   ]
 )
-
 
 print(completion.choices[0].message)
