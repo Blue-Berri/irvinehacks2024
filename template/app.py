47a03d67fa272e330cd1a538f705dbade6cc3a61
Justin Ho <justin.ho05072005@gmail.com>
2024-01-27 21:32:45 -0800

    changes to profile editor /reg
    untested

--- template/app.py
+++ template/app.py
@@ -64,7 +64,7 @@ def profileeditor():
 
         return redirect(url_for('home'))
 
-    return render_template("register.html")
+    return render_template("profileeditor.html")
 
 
 
@@ -105,7 +105,7 @@ def register():
             file.write(users)
 
 
-        return redirect(url_for('home'))
+        return redirect(url_for('profileeditor'))
 
     return render_template("register.html")
 
