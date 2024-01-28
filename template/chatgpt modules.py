14f4788b19fdb9e55f6e2a8b2d7da0515512a97f
Justin Ho <justin.ho05072005@gmail.com>
2024-01-28 11:51:57 -0800

    chatgpt api

--- template/chatgpt modules.py
+++ template/chatgpt modules.py
@@ -24,27 +24,27 @@ current = {
     "orientation": item["orientation"]
 }
 
-# completion = client.chat.completions.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {
-#       "role": "system", 
-#       "content": f'''You are a matchmaker. I will give you a set of traits about me and give you a
-#         list of possible matches in the json file {new_data}. Match gender and sexual orientation as the
-#         highest priority with a weight of over 90%. If orientation is straight that means males like females 
-#         and females like males. 
-#         Your job is to find the best matches for the person. Go through the entire list of data in the json.
-#         Refer to the use in the you form. Give back list of only names in order from highest match to 
-#         lowest match. python list of emails. The list must have at least 15 emails. format it as [emails]. dont include 
-#       other words '''
-#      },
-#     {
-#       "role": "user", 
-#       "content": f'''Here is a json file {current} of your information. Only consider the factors stated above.
-#       return in python list of emails. The list must have at least 15 emails. format it as [emails]  do not include any other words '''
-#     }
-#   ]
-# )
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "system", 
+      "content": f'''You are a matchmaker. I will give you a set of traits about me and give you a
+        list of possible matches in the json file {new_data}. Match gender and sexual orientation as the
+        highest priority with a weight of over 90%. If orientation is straight that means males like females 
+        and females like males. 
+        Your job is to find the best matches for the person. Go through the entire list of data in the json.
+        Refer to the use in the you form. Give back list of only names in order from highest match to 
+        lowest match. python list of emails. The list must have at least 15 emails. format it as [emails]. dont include 
+      other words '''
+     },
+    {
+      "role": "user", 
+      "content": f'''Here is a json file {current} of your information. Only consider the factors stated above.
+      return in python list of emails. The list must have at least 15 emails. format it as [emails]  do not include any other words '''
+    }
+  ]
+)
 
 results = ["r@uci","taylor@abc.edu","emily@gmail.com","tae@awesome.cool"]
 
