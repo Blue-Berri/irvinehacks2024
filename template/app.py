fdf11f0d1a4ae0a7927b371cd2ac2b734d2405ce
Justin Ho <justin.ho05072005@gmail.com>
2024-01-27 20:52:06 -0800

    created current feature and dictionary for data

--- template/app.py
+++ template/app.py
@@ -47,39 +47,32 @@ def register():
             "pfp": None
         }
 
+
         try:
             with open("template/userData.json", 'r') as file:
                 existing_data = json.loads(file.read())
                 
         except (FileNotFoundError, json.decoder.JSONDecodeError):
             existing_data = {
+                "current":{
+
+                },
                 "users": [
 
                 ]
             }
         existing_data["users"].append(data)
+        existing_data["current"] = data
         with open('template/userData.json', 'w') as file:
             users = json.dumps(existing_data)
             file.write(users)
+
+
         return redirect(url_for('home'))
 
     return render_template("register.html")
 
-# @app.route("/profileeditor", methods=["POST", "GET"])
-# def profileeditor():
-#     if request.method == "POST":
-#         # import pdb; pdb.set_trace()
-#         data = {
-#             "email": request.form['emailInput'],
-#             "password": request.form['passwordInput'],
-#             "name": None,
-#             "age": None,
-#             "gender": None,
-#             "orientation": None,
-#             "major": None,
-#             "bio": None,
-#             "pfp": None
-#         }
+
 
 
 if __name__ == "__main__":
