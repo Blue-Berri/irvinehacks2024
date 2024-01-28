ce49420da58c64d373687f017f250a7bd8b6b009
Justin Ho <justin.ho05072005@gmail.com>
2024-01-27 18:32:53 -0800

    navigate through html pages in app.py

--- template/app.py
+++ template/app.py
@@ -15,20 +15,36 @@ def home():
 def matches():
     return render_template("matches.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/messages")
+def messages():
+    return render_template("messages.html")
+
+@app.route("/profileeditor")
+def profileeditor():
+    return render_template("profileeditor.html")
+
 @app.route("/register", methods=["POST"])
 def register():
     print("Hi")
     if request.method == "POST":
         data = {
             "email": request.form['Email'],
-            "password": request.form['Password'],
-            "name": request.form['Name'],
-            "age": request.form['Age'],
-            "gender": request.form['Gender'],
-            "orientation": request.form['Orientation'],
-            "major": request.form['Major'],
-            "bio": request.form['Bio'],
-            "pfp": request.form['Pfp']
+            "password": request.form['Password']
+            # "name": request.form['Name'],
+            # "age": request.form['Age'],
+            # "gender": request.form['Gender'],
+            # "orientation": request.form['Orientation'],
+            # "major": request.form['Major'],
+            # "bio": request.form['Bio'],
+            # "pfp": request.form['Pfp']
         }
         print(data)
         try:
@@ -37,8 +53,8 @@ def register():
         except FileNotFoundError:
             existing_data = []
         existing_data.append(data)
-    with open('userData.json', 'w') as file:
-        json.dump(existing_data, file)
+        with open('userData.json', 'w') as file:
+            json.dump(existing_data, file)
     
 # put data in db
     return redirect(url_for('home'))
